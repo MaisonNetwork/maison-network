bb5227a34989ba2b2b3ce762d460371c4451a472
George Bennett <george6bennett@gmail.com>
2018-09-10 21:31:43 +0100

    add RangeSlider

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -18,6 +18,7 @@ import {
   VToolbar,
   VDialog,
   VTabs,
+  VRangeSlider,
   VSlider,
   VStepper,
   VSelect
@@ -42,6 +43,7 @@ Vue.use(Vuetify, {
     VToolbar,
     VDialog,
     VTabs,
+    VRangeSlider,
     VSlider,
     VSelect,
     VStepper
